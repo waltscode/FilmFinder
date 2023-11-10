77fbb444365f515c4f3478e86173c45eb8734b32
Austin Graham <austiingraham@gmail.com>
2023-11-09 21:30:12 -0500

    added feature to display popular movies on homepage, search results now show 12 movies instead of only 1, other small improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,59 @@
+// Execute code when the document is fully loaded
+$(document).ready(function() {
+
 // Selecting elements using jQuery
 var btnEl = $(".btn");
 var movieDetailsContainer = $("#movie-details-container");
 
-btnEl.on("click", function (event) {
-  event.preventDefault(); // Prevent the default behavior of the button
+  // Attach a click event handler to the button element
+  btnEl.on('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior of the button
 
-  // Get the user's input for the movie name
-  var inputValue = $("#movie-name");
+    // Get the user's input for the movie name
+    var inputValue = $('#movie-name');
 
-  // Construct the API request URL with the user's input
-  var requestUrl = `https://api.themoviedb.org/3/search/movie?api_key=b959be3036efe07cdd94c9fb04a40299&query=${inputValue.val()}`;
+    // Construct the API request URL with the user's input
+    var requestUrl = `https://api.themoviedb.org/3/search/movie?api_key=b959be3036efe07cdd94c9fb04a40299&query=${inputValue.val()}`;
 
-  // Fetch data from the API using the constructed URL
-  fetch(requestUrl)
-    .then(function (response) {
-      console.log(response);
-      if (response.status === 404) {
-        // Handle 404 response
-        throw new Error("Movie not found");
-      }
-      return response.json(); // Parse the response as JSON
-    })
-    .then(function (data) {
-      console.log(data);
-      // Check if there's a movie result
-      if (data.results.length > 0) {
-        var movie = data.results[0]; // Take the first result
-        createMovieElement(movie, movieDetailsContainer);
+    // Fetch data from the API using the constructed URL
+    fetch(requestUrl)
+      .then(function(response) {
+        console.log(response);
+        if (response.status === 404) {
+          // Handle 404 response
+          throw new Error('Movie not found');
+        }
+        return response.json(); // Parse the response as JSON
+      })
+      .then(function(data) {
+        console.log(data);
+        // Check if there's a movie result
+        if (data.results.length > 0) {
+          var moviesContainer = $('<div class="movies-container"></div>');
 
-        // Clear the search input
-        inputValue.val(""); // Clear the input value
-      } else {
-        // Handle the case where no movie is found
-        movieDetailsContainer.html("No movie found.");
-      }
-    })
-    .catch(function (err) {
-      console.log(err.message); // Handle any errors, including 404
-      if (err.message === "Movie not found") {
-        movieDetailsContainer.html("Movie not found.");
-      } else {
-        // Handle other errors
-      }
-    });
-});
+          // Loop through the first 12 results or less
+          for (var i = 0; i < Math.min(12, data.results.length); i++) {
+            createMovieElement(data.results[i], moviesContainer);
+          }
+
+          movieDetailsContainer.html(moviesContainer);
+          inputValue.val(''); // Clear the input value
+          popularMoviesContainer.html('');
+          movieGenresEl.html('');
+        } else {
+          movieDetailsContainer.html('No movies found.');
+        }
+
+      })
+      .catch(function(err) {
+        console.log(err.message); // Handle any errors, including 404
+        if (err.message === 'Movie not found') {
+          movieDetailsContainer.html('Movie not found.');
+        } else {
+          // Handle other errors
+        }
+      });
+  });
 
 // Function to create and append elements to display movie details
 function createMovieElement(movie, container) {
@@ -62,7 +72,7 @@ function createMovieElement(movie, container) {
   var detailsBox = $('<div class="details-box"></div>');
   var title = $("<h2>").text(movie.title);
   var description = $("<p>").text(movie.overview);
-  detailsBox.append(title, description);
+  detailsBox.append(title);
   // Create a button to add the movie to the watchlist
   var addToWatchlistBtn = $(
     '<button class="add-to-watchlist-btn">Add to Watchlist</button>'
@@ -75,7 +85,7 @@ function createMovieElement(movie, container) {
   movieCard.append(posterBox, detailsBox, addToWatchlistBtn);
 
   // Append the movie card to the container
-  container.html(movieCard);
+  container.append(movieCard);
 }
 
 var movieGenresEl = $(".movie-genres");
@@ -123,7 +133,7 @@ function fetchAndDisplayMovies(genre, genreId) {
           var detailsBox = $('<div class="details-box"></div>');
           var title = $("<h2>").text(movie.title);
           var description = $("<p>").text(movie.overview);
-          detailsBox.append(title, description);
+          detailsBox.append(title);
           var addToWatchlistBtn = $(
             '<button class="add-to-watchlist-btn">Add to Watchlist</button>'
           );
@@ -173,66 +183,42 @@ function addToWatchlist(movie) {
   watchlist.push(movie);
   // Save the updated watchlist back to local storage
   localStorage.setItem("watchlist", JSON.stringify(watchlist));
-
-  //////////////
-  /*
-// Selecting elements using jQuery
-var btnEl = $('.btn');
-var movieDetailsContainer = $('#movie-details-container');
-var inputValue = $('#movie-name'); // Get the user's input field
-
-btnEl.on('click', function (event) {
-  event.preventDefault(); // Prevent the default behavior of the button
-  handleSearch();
-});
-
-// Function to handle the search
-function handleSearch() {
-  // Get the user's input for the movie name
-  var movieName = inputValue.val();
-
-  // Construct the API request URL with the user's input
-  var requestUrl = `https://api.themoviedb.org/3/search/movie?api_key=b959be3036efe07cdd94c9fb04a40299&query=${movieName}`;
-
-  // Fetch data from the API using the constructed URL
-  fetch(requestUrl)
-    .then(function (response) {
-      console.log(response);
-      if (response.status === 404) {
-        // Handle 404 response
-        throw new Error('Movie not found');
-      }
-      return response.json(); // Parse the response as JSON
-    })
-    .then(function (data) {
-      console.log(data);
-      // Check if there's a movie result
-      if (data.results.length > 0) {
-        var movie = data.results[0]; // Take the first result
-        createMovieElement(movie, movieDetailsContainer);
-
-        // Clear the search input
-        inputValue.val(''); // Clear the input value
-      } else {
-        // Handle the case where no movie is found
-        movieDetailsContainer.html('No movie found.');
-      }
-    })
-    .catch(function (err) {
-      console.log(err.message); // Handle any errors, including 404
-      if (err.message === 'Movie not found') {
-        movieDetailsContainer.html('Movie not found.');
-      } else {
-        // Handle other errors
-      }
-    });
 }
 
-// Listen for "Enter" key press in the input field
-inputValue.keyup(function (event) {
-  if (event.keyCode === 13) {
-    // If the "Enter" key is pressed, handle the search
-    handleSearch();
+  // --- Function for displaying popular movies --- //
+  var popularMoviesContainer = $('#popular-movies-container .popular-movies');
+
+  // Function to fetch and display popular movies
+  function fetchAndDisplayPopularMovies() {
+    var popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=b959be3036efe07cdd94c9fb04a40299`;
+
+    fetch(popularMoviesUrl)
+      .then(function (response) {
+        if (response.status === 404) {
+          throw new Error('Popular movies not found');
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.results.length > 0) {
+          var moviesContainer = $('<div class="movies-container"></div>');
+          for (var i = 0; i < Math.min(4, data.results.length); i++) {
+            createMovieElement(data.results[i], moviesContainer);
+          }
+          popularMoviesContainer.html(moviesContainer);
+        } else {
+          popularMoviesContainer.html('No popular movies found.');
+        }
+      })
+      .catch(function (err) {
+        console.log(err.message);
+        if (err.message === 'Popular movies not found') {
+          popularMoviesContainer.html('No popular movies found.');
+        } else {
+          // Handle other errors
+        }
+      });
   }
-}); */
-}
+
+  fetchAndDisplayPopularMovies();
+});
