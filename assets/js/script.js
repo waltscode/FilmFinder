a6e8bb8e1a6aef92c20703ef12a269536efbcd37
Samm1911 <s_gostanian@hotmail.com>
2023-11-09 19:43:07 -0500

    adding watclist buttons-clear all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Selecting elements using jQuery
-var btnEl = $('.btn');
-var movieDetailsContainer = $('#movie-details-container');
+var btnEl = $(".btn");
+var movieDetailsContainer = $("#movie-details-container");
 
-btnEl.on('click', function (event) {
+btnEl.on("click", function (event) {
   event.preventDefault(); // Prevent the default behavior of the button
 
   // Get the user's input for the movie name
-  var inputValue = $('#movie-name');
+  var inputValue = $("#movie-name");
 
   // Construct the API request URL with the user's input
   var requestUrl = `https://api.themoviedb.org/3/search/movie?api_key=b959be3036efe07cdd94c9fb04a40299&query=${inputValue.val()}`;
@@ -17,7 +17,7 @@ btnEl.on('click', function (event) {
       console.log(response);
       if (response.status === 404) {
         // Handle 404 response
-        throw new Error('Movie not found');
+        throw new Error("Movie not found");
       }
       return response.json(); // Parse the response as JSON
     })
@@ -27,18 +27,18 @@ btnEl.on('click', function (event) {
       if (data.results.length > 0) {
         var movie = data.results[0]; // Take the first result
         createMovieElement(movie, movieDetailsContainer);
-        
+
         // Clear the search input
-        inputValue.val(''); // Clear the input value
+        inputValue.val(""); // Clear the input value
       } else {
         // Handle the case where no movie is found
-        movieDetailsContainer.html('No movie found.');
+        movieDetailsContainer.html("No movie found.");
       }
     })
     .catch(function (err) {
       console.log(err.message); // Handle any errors, including 404
-      if (err.message === 'Movie not found') {
-        movieDetailsContainer.html('Movie not found.');
+      if (err.message === "Movie not found") {
+        movieDetailsContainer.html("Movie not found.");
       } else {
         // Handle other errors
       }
@@ -52,20 +52,24 @@ function createMovieElement(movie, container) {
 
   // Create a box to hold the movie poster
   var posterBox = $('<div class="poster-box"></div>');
-  var image = $('<img>').attr('src', 'https://image.tmdb.org/t/p/w200' + movie.poster_path);
+  var image = $("<img>").attr(
+    "src",
+    "https://image.tmdb.org/t/p/w200" + movie.poster_path
+  );
   posterBox.append(image);
-  
 
   // Create a box for movie details (title and description)
   var detailsBox = $('<div class="details-box"></div>');
-  var title = $('<h2>').text(movie.title);
-  var description = $('<p>').text(movie.overview);
+  var title = $("<h2>").text(movie.title);
+  var description = $("<p>").text(movie.overview);
   detailsBox.append(title, description);
-    // Create a button to add the movie to the watchlist
-    var addToWatchlistBtn = $('<button class="add-to-watchlist-btn">Add to Watchlist</button>');
-    addToWatchlistBtn.on('click', function() {
-      addToWatchlist(movie);
-    });
+  // Create a button to add the movie to the watchlist
+  var addToWatchlistBtn = $(
+    '<button class="add-to-watchlist-btn">Add to Watchlist</button>'
+  );
+  addToWatchlistBtn.on("click", function () {
+    addToWatchlist(movie);
+  });
 
   // Append the poster box and details box to the movie card
   movieCard.append(posterBox, detailsBox, addToWatchlistBtn);
@@ -74,16 +78,16 @@ function createMovieElement(movie, container) {
   container.html(movieCard);
 }
 
-var movieGenresEl = $('.movie-genres');
+var movieGenresEl = $(".movie-genres");
 var genreBtns = [
-  { id: '#action-btn', genreId: 28 },
-  { id: '#adventure-btn', genreId: 12 },
-  { id: '#comedy-btn', genreId: 35 },
-  { id: '#drama-btn', genreId: 18 },
-  { id: '#horror-btn', genreId: 27 },
-  { id: '#romance-btn', genreId: 10749 },
-  { id: '#thriller-btn', genreId: 53 },
-  { id: '#western-btn', genreId: 37 },
+  { id: "#action-btn", genreId: 28 },
+  { id: "#adventure-btn", genreId: 12 },
+  { id: "#comedy-btn", genreId: 35 },
+  { id: "#drama-btn", genreId: 18 },
+  { id: "#horror-btn", genreId: 27 },
+  { id: "#romance-btn", genreId: 10749 },
+  { id: "#thriller-btn", genreId: 53 },
+  { id: "#western-btn", genreId: 37 },
 ];
 
 function fetchAndDisplayMovies(genre, genreId) {
@@ -96,7 +100,7 @@ function fetchAndDisplayMovies(genre, genreId) {
       console.log(response);
       if (response.status === 404) {
         // Handle 404 response
-        throw new Error('Movies not found');
+        throw new Error("Movies not found");
       }
       return response.json(); // Parse the JSON response
     })
@@ -110,16 +114,22 @@ function fetchAndDisplayMovies(genre, genreId) {
         data.results.forEach(function (movie) {
           var movieCard = $('<div class="movie-card"></div>');
           var posterBox = $('<div class="poster-box"></div>');
-          var image = $('<img>').attr('src', 'https://image.tmdb.org/t/p/w200' + movie.poster_path);
+          var image = $("<img>").attr(
+            "src",
+            "https://image.tmdb.org/t/p/w200" + movie.poster_path
+          );
           posterBox.append(image);
 
           var detailsBox = $('<div class="details-box"></div>');
-          var title = $('<h2>').text(movie.title);
-          var description = $('<p>').text(movie.overview);
+          var title = $("<h2>").text(movie.title);
+          var description = $("<p>").text(movie.overview);
           detailsBox.append(title, description);
-          var addToWatchlistBtn = $('<button class="add-to-watchlist-btn">Add to Watchlist</button>');
-          addToWatchlistBtn.on('click', function() {
+          var addToWatchlistBtn = $(
+            '<button class="add-to-watchlist-btn">Add to Watchlist</button>'
+          );
+          addToWatchlistBtn.on("click", function () {
             addToWatchlist(movie);
+            this.innerText = "Added to Watchlist";
           });
 
           movieCard.append(posterBox, detailsBox, addToWatchlistBtn);
@@ -130,13 +140,13 @@ function fetchAndDisplayMovies(genre, genreId) {
         genre.html(moviesContainer);
       } else {
         // Handle the case where no movies are found
-        genre.html('No movies found.');
+        genre.html("No movies found.");
       }
     })
     .catch(function (err) {
       console.log(err.message); // Handle any errors, including 404
-      if (err.message === 'Movies not found') {
-        genre.html('No movies found.');
+      if (err.message === "Movies not found") {
+        genre.html("No movies found.");
       } else {
         // Handle other errors
       }
@@ -144,7 +154,7 @@ function fetchAndDisplayMovies(genre, genreId) {
 }
 
 genreBtns.forEach(function (button) {
-  $(button.id).on('click', function (event) {
+  $(button.id).on("click", function (event) {
     event.preventDefault();
     fetchAndDisplayMovies(movieGenresEl, button.genreId);
   });
@@ -152,30 +162,20 @@ genreBtns.forEach(function (button) {
 
 function addToWatchlist(movie) {
   // Retrieve the existing watchlist from local storage (if any)
-  var watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
+  var watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
 
   // Check if the movie is already in the watchlist
   var isMovieInWatchlist = watchlist.some(function (item) {
     return item.id === movie.id;
   });
 
-  if (!isMovieInWatchlist) {
-    // Add the movie to the watchlist
-    watchlist.push(movie);
+  // Add the movie to the watchlist
+  watchlist.push(movie);
+  // Save the updated watchlist back to local storage
+  localStorage.setItem("watchlist", JSON.stringify(watchlist));
 
-    // Save the updated watchlist back to local storage
-    localStorage.setItem('watchlist', JSON.stringify(watchlist));
-
-     // Provide feedback to the user
-     alert('Movie added to Watchlist!');
-  } else {
-    // If the movie is already in the watchlist, inform the user
-    alert('Movie is already in your Watchlist.');
-  }
-}
-
-////////////// 
-/*
+  //////////////
+  /*
 // Selecting elements using jQuery
 var btnEl = $('.btn');
 var movieDetailsContainer = $('#movie-details-container');
@@ -235,3 +235,4 @@ inputValue.keyup(function (event) {
     handleSearch();
   }
 }); */
+}
